34ca7fdc0794aca5244d2d37442345528a121be4
Emplic <89635087+Emplic@users.noreply.github.com>
2024-09-11 12:48:27 +0300

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,2 +1,2 @@
 -- dont skid
-loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/204eaba40718e1c6d22eac1688d5be26.lua"))()
+loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/Emplic/NOBULEM/main/loader.lua"))()
