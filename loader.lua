1c6e35751cb4593d6587f09926441eda0a21257b
Emplic <89635087+Emplic@users.noreply.github.com>
2024-12-06 11:29:51 +0200

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,3 +1,1 @@
--- solara supported
-
-loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/08425852ab0dce3be023783ff9269da0.lua"))()
+game.Players.LocalPlayer:Kick("nblm.cc is discontinued.")
