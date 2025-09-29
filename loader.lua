c69bd257d90bb3da7c6c61166326181e35fde314
Emplic <89635087+Emplic@users.noreply.github.com>
2025-09-29 21:36:31 +0300

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/58b9150f22a79b4e46fcc2097eeb5e45.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Nobulem/wtf/refs/heads/main/loader.lua"))()
